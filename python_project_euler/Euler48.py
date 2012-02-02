34ca73869bc7ff09c28b02c4c06bea60adb81c56
Derek Passen <dpassen1@gmail.com>
2012-02-01 22:19:59 -0500

    Cleaner way of excluding 0**0 from the series

--- python_project_euler/Euler48.py
+++ python_project_euler/Euler48.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python -tt
 
 def series_generator(bound):
-    return (num ** num for num in range(bound + 1) if num)
+    return (num ** num for num in range(1, bound + 1))
 
 example = series_generator(1000)
 print(str(sum(example))[-10:])
