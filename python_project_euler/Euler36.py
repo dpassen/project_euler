d34c8a3f647b17f5478dfb194033c0f86ce047e2
Derek Passen <dpassen1@gmail.com>
2012-02-01 19:19:48 -0500

    Easy brute force solution to Euler 36

---
+++ python_project_euler/Euler36.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python -tt
+
+def is_palindrome(value):
+    str_value = str(value)
+    return str_value == str_value[::-1]
+
+print(sum(num for num in range(1000000) if is_palindrome(num) and is_palindrome(bin(num)[2:])))
