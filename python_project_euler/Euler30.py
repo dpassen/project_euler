7f3d9fb4c19951cb599eeaf3393f09bcc09f7564
Derek Passen <dpassen1@gmail.com>
2012-02-04 15:37:10 -0500

    Euler30 was very similar to Euler34. All the way down to the fact that the upper bound is nebulous to me.

---
+++ python_project_euler/Euler30.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python -tt
+
+def sum_of_nth_power_of_digits(n, num):
+    return sum(int(digit) ** n for digit in str(num))
+
+def generate_nth_power_equalities(n, upper_bound):
+    for num in range(2, upper_bound):
+        if num == sum_of_nth_power_of_digits(n, num): 
+            yield num
+
+print(sum(generate_nth_power_equalities(5, 1000000)))
