57cbce7ce4b195a1ef0c47fa33a010f6626f42b1
Derek Passen <dpassen1@gmail.com>
2012-02-03 23:21:34 -0500

    Euler34 solution. I kind of guessed at the upper bound. Not really sure how to figure out a mathematically sound upper bound.

---
+++ python_project_euler/Euler34.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python -tt
+
+from math import factorial
+
+def generate_curious_numbers_up_to(upper_bound):
+    for num in range(3, upper_bound):
+        if num == sum(factorial(int(digit)) for digit in str(num)):
+            yield num
+
+print(sum(generate_curious_numbers_up_to(1000000)))
