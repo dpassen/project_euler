1c2738ec5fe0f7b24bb346826a211b5f2cbfb3fa
Derek Passen <dpassen1@gmail.com>
2012-05-22 19:24:19 -0400

    Euler 19.

---
+++ python_project_euler/Euler19.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python -tt
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+date = datetime(1901,01,01)
+count = 0
+
+while date.year < 2001:
+    if date.isoweekday() == 7:
+        count += 1
+    date += relativedelta(months = 1)
+
+print(count)
+
