1bb59317e72a67951b1657ef449b2d9f7e5ffe8b
Derek Passen <dpassen1@gmail.com>
2012-05-21 23:01:01 -0400

    Euler 18 and 67 solutions.
    Did these at the same time, so as to use a quick algorithm.
    Not in love with my indices into the 2-dimensional array,
    but enumerating from [1:] looked worse. Undecided.

---
+++ python_project_euler/Euler67.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python -tt
+
+with open('data_files/triangle.txt') as f:
+    triangle = f.read().strip().split('\n')
+    triangle = [[int(num) for num in row.split()] for row in triangle]
+    triangle.reverse()
+
+    for row_index, row in enumerate(triangle):
+        if row_index != 0:
+            for index, num in enumerate(row):
+                triangle[row_index][index] += max(triangle[row_index - 1][index], triangle[row_index - 1][index + 1])
+
+    print(triangle[-1][0])
+
