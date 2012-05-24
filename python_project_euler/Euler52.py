f1e0620ac07f569f7b2c214f67461f96436e0545
Derek Passen <dpassen1@gmail.com>
2012-05-24 19:37:35 -0400

    More readable, more pythonic Euler 52.

--- python_project_euler/Euler52.py
+++ python_project_euler/Euler52.py
@@ -2,8 +2,8 @@
 
 from itertools import count
 
-for num in count(1):
-    if (set(str(num)) == set(str(num * 2)) == set(str(num * 3)) == set(str(num * 4)) == set(str(num * 5)) == set(str(num * 6))):
-        break
-print(num)
+def contains_same_terms_as_multiples(num):
+    return set(str(num)) == set(str(num * 2)) == set(str(num * 3)) == set(str(num * 4)) == set(str(num * 5)) == set(str(num * 6))
+
+print(next(num for num in count(1) if contains_same_terms_as_multiples(num)))
 
