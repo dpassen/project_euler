e8019f8f35a401124339d1ac47036145bde9304e
Derek Passen <dpassen1@gmail.com>
2014-12-06 09:20:16 -0600

    Even more consistent Clojure.

--- clojure_project_euler/project-25/src/project_25/core.clj
+++ clojure_project_euler/project-25/src/project_25/core.clj
@@ -3,8 +3,7 @@
 (def fibs (map first (iterate (fn [[a b]] [b (+ a b)]) [0M 1M])))
 
 (defn -main [& args]
-  (->>
-    fibs
-    (take-while (comp (partial > 1000) count str))
-    count
-    println))
+  (->> fibs
+       (take-while (comp (partial > 1000) count str))
+       count
+       println))
