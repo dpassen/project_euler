e9c3082cf286121c358bc8d830150d098c92e385
Derek Passen <dpassen1@gmail.com>
2014-12-28 19:02:46 -0600

    Upgrade numeric-tower for Clojure Euler 5.

--- clojure_project_euler/project-5/project.clj
+++ clojure_project_euler/project-5/project.clj
@@ -1,5 +1,5 @@
 (defproject project-5 "1.0.0-SNAPSHOT"
   :description "Project Euler 5 in Clojure"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/math.numeric-tower "0.0.1"]]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :main project-5.core)
