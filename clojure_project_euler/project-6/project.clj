31e4589d210820a761971f68771fdd92fe05e42d
Derek Passen <dpassen1@gmail.com>
2014-12-29 19:34:39 -0600

    Euler 6 in Clojure.

---
+++ clojure_project_euler/project-6/project.clj
@@ -0,0 +1,5 @@
+(defproject project-6 "1.0.0-SNAPSHOT"
+  :description "Project Euler 6 in Clojure"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :main project-6.core)
---
+++ clojure_project_euler/project-6/src/project_6/core.clj
@@ -0,0 +1,16 @@
+(ns project-6.core
+  (:require [clojure.math.numeric-tower :refer [abs expt]]))
+
+(defn- square [x] (expt x 2))
+
+(defn -main [& args]
+  (let [nums (range 1 101)
+        sum-of-squares (->> nums
+                            (map square)
+                            (reduce +))
+        square-of-sums (->> nums
+                            (reduce +)
+                            square)]
+    (->> (- sum-of-squares square-of-sums)
+         abs
+         println)))
