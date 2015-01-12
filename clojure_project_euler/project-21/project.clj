a9735118a40cf383dfe36aa8d0eb78605234c8be
Derek Passen <dpassen1@gmail.com>
2015-01-11 20:31:55 -0600

    Euler 21 in Clojure.

---
+++ clojure_project_euler/project-21/project.clj
@@ -0,0 +1,5 @@
+(defproject project-21 "1.0.0-SNAPSHOT"
+  :description "Project Euler 21 in Clojure"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :main project-21.core)
---
+++ clojure_project_euler/project-21/src/project_21/core.clj
@@ -0,0 +1,24 @@
+(ns project-21.core
+  (:require [clojure.math.numeric-tower :refer [floor sqrt]]))
+
+(defn divisors [num]
+  (->> (for [x (range 1 (inc (floor (sqrt num))))
+             :when (zero? (mod num x))]
+         [x (/ num x)])
+       flatten
+       distinct))
+
+(defn proper-divisors [num]
+  (remove #{num} (divisors num)))
+
+(defn amicable? [num]
+  (let [pair (reduce + (proper-divisors num))]
+    (and
+     (not= num pair)
+     (= num (reduce + (proper-divisors pair))))))
+
+(defn -main [& args]
+  (->> (range 1 10000)
+       (filter amicable?)
+       (reduce +)
+       println))
