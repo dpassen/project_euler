5cb34f832c82f10be29b578d6f1e065332bfc737
Derek Passen <dpassen1@gmail.com>
2015-01-18 20:09:35 -0600

    Euler 27 in Clojure.

---
+++ clojure_project_euler/project-27/project.clj
@@ -0,0 +1,5 @@
+(defproject project-27 "1.0.0-SNAPSHOT"
+  :description "Project Euler 27 in Clojure"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :main project-27.core)
---
+++ clojure_project_euler/project-27/src/project_27/core.clj
@@ -0,0 +1,27 @@
+(ns project-27.core
+  (:require [clojure.math.numeric-tower :refer [floor sqrt]]))
+
+(defn prime? [num]
+  (cond
+    (>= 1 num) false
+    :else (empty? (for [x (range 2 (inc (floor (sqrt num))))
+                        :when (zero? (mod num x))]
+                    x))))
+
+(def primes (filter prime? (range)))
+
+(defn quadratic-primes [a b]
+  (->> (range)
+       (map (fn [n] (+ (* n n) (* a n) b)))
+       (take-while prime?)))
+
+(defn -main [& args]
+  (->> (for [a (range -1000 1001)
+             b (take-while (partial > 1001) primes)] ;for n^2 + an + b to be prime when n = 0,
+                                                     ;b must be prime
+         [a b])
+       (map (juxt identity (comp count (partial apply quadratic-primes))))
+       (apply max-key second)
+       first
+       (reduce *)
+       println))
