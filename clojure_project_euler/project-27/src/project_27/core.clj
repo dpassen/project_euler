8b405e9e27f8e25bf3dd3a23342168ed26d6ae21
Derek Passen <dpassen1@gmail.com>
2015-07-15 17:30:36 -0500

    Simply Clojure 27.

--- clojure_project_euler/project-27/src/project_27/core.clj
+++ clojure_project_euler/project-27/src/project_27/core.clj
@@ -2,11 +2,11 @@
   (:require [clojure.math.numeric-tower :refer [floor sqrt]]))
 
 (defn prime? [num]
-  (cond
-    (>= 1 num) false
-    :else (empty? (for [x (range 2 (inc (floor (sqrt num))))
-                        :when (zero? (mod num x))]
-                    x))))
+  (when (< 1 num)
+    (empty?
+     (for [x (range 2 (inc (floor (sqrt num))))
+           :when (zero? (mod num x))]
+       x))))
 
 (def primes (filter prime? (range)))
 
