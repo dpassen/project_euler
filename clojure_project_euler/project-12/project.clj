8d3295b60d60a607ef6236606952c1f82f80351e
Derek Passen <dpassen1@gmail.com>
2017-05-15 17:23:37 -0500

    Euler 12 in Clojure.

---
+++ clojure_project_euler/project-12/project.clj
@@ -0,0 +1,5 @@
+(defproject project-12 "1.0.0-SNAPSHOT"
+  :description "Project Euler 12 in Clojure"
+  :dependencies [[org.clojure/clojure            "1.8.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :main project-12.core)
