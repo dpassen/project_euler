41fbf1deed1045b3b33a4da17b8529674af05181
Derek Passen <dpassen1@gmail.com>
2017-05-15 19:16:48 -0500

    Project Euler 9 in Clojure.

---
+++ clojure_project_euler/project-9/project.clj
@@ -0,0 +1,5 @@
+(defproject project-9 "1.0.0-SNAPSHOT"
+  :description "Project Euler 9 in Clojure"
+  :dependencies [[org.clojure/clojure            "1.8.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :main project-9.core)
