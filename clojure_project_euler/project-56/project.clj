b46ea7953997cb07a03d1366a290e093f08f72fb
Derek Passen <dpassen1@gmail.com>
2017-07-29 19:18:19 -0500

    Project Euler 56 in Clojure.

---
+++ clojure_project_euler/project-56/project.clj
@@ -0,0 +1,6 @@
+(defproject project-56 "1.0.0-SNAPSHOT"
+  :description "Project Euler 56 in Clojure"
+  :dependencies [[org.clojure/clojure            "1.8.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [net.cgrand/xforms              "0.9.2"]]
+  :main project-56.core)
