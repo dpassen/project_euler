53a3e24b84d174c95e67b6133634bbb5b41d8153
Derek Passen <dpassen1@gmail.com>
2017-12-01 22:51:45 -0600

    Much faster Clojure 56 with Longs rather than BigInts.

--- clojure_project_euler/project-56/src/project_56/core.clj
+++ clojure_project_euler/project-56/src/project_56/core.clj
@@ -4,10 +4,10 @@
    [net.cgrand.xforms.rfs      :as rfs]))
 
 (defn -main [& args]
-  (letfn [(digital-sum [n] (transduce (comp (map str) (map bigint)) + (str n)))]
+  (letfn [(digital-sum [n]
+            (transduce (comp (map str) (map #(Long/parseLong %))) + (str n)))]
     (->> (for [a (range 1 101)
                b (range 1 101)]
            (expt a b))
          (transduce (map digital-sum) rfs/max)
-         str
          println)))
