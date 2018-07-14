154b81857192988c868c7fb620fea92efcd26dca
Derek Passen <dpassen1@gmail.com>
2018-07-13 20:47:51 -0500

    This was bothering me.

--- python_project_euler/Euler14.py
+++ python_project_euler/Euler14.py
@@ -2,10 +2,9 @@
 
 
 def generate_seq(seed):
-    seq = []
-    seq.append(seed)
+    seq = [seed]
     while seed > 1:
-        seed = (3 * seed + 1) if (seed % 2) else (seed // 2)
+        seed = (3 * seed + 1) if seed % 2 else seed // 2
         seq.append(seed)
     return seq
 
