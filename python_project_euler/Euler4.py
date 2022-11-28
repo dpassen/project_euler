e6a405b342f82e709be29d3c7f8ea218f38edd1e
Derek Passen <dpassen1@gmail.com>
2022-11-28 12:36:40 -0600

    Remove unnecessary parens in Python solution to problem four

--- python_project_euler/Euler4.py
+++ python_project_euler/Euler4.py
@@ -2,11 +2,9 @@
 
 print(
     max(
-        (
-            i * j
-            for i in range(100, 1000)
-            for j in range(100, 1000)
-            if str(i * j) == str(i * j)[::-1]
-        )
+        i * j
+        for i in range(100, 1000)
+        for j in range(100, 1000)
+        if str(i * j) == str(i * j)[::-1]
     )
 )
