3116365dc01ac3ccf9d12b7b7a68cf7714898dac
Derek Passen <dpassen1@gmail.com>
2023-09-26 19:19:45 -0500

    Remove unnecessary iterate

--- clojure_project_euler/project-52/src/project_52.clj
+++ clojure_project_euler/project-52/src/project_52.clj
@@ -7,5 +7,6 @@
     (filter
      (fn [nums] (apply = (map (comp set str) nums)))
      (eduction
+      (map inc)
       (map (fn [n] (map (partial * n) (range 1 7))))
-      (iterate inc 1))))))
+      (range))))))
